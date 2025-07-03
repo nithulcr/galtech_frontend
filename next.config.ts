60be5233978fc238e3ce99aa6acf64b3930d3bcd
nithul <nithulcr@gmail.com>
2025-07-03 10:35:52 +0530

    first commit9

--- next.config.ts
+++ next.config.ts
@@ -4,28 +4,10 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '1337',
-        pathname: '/uploads/**',
-      },
-      {
-        protocol: 'http',
-        hostname: '127.0.0.1',
-        port: '1337',
-        pathname: '/uploads/**',
-      },
-      {
-        protocol: 'http',
-        hostname: '139.84.145.64',
-        port: '1337',
-        pathname: '/uploads/**',
-      },
-      {
         protocol: 'https',
         hostname: 'galtechstarpi-production.up.railway.app',
         pathname: '/uploads/**',
-      }
+      },
     ],
   },
 };
