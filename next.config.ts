d1a20e481f04f9bc161580219bf00fd771f390b7
nithul <nithulcr@gmail.com>
2025-07-03 14:44:28 +0530

    test3

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,12 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'galtechstarpi-production.up.railway.app',
-        pathname: '/uploads/**',
+        pathname: '/uploads/**', // still valid for old images if needed
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**', // allow all paths under Cloudinary (required!)
       },
     ],
   },
